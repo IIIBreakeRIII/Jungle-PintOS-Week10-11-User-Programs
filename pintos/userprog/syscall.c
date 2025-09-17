06a3237a4a70f9399c5f841d18d1c1b48018471c
IIIBreakeRIII <ryu990305@icloud.com>
2025-09-17 15:28:47 +0900

    [Bug-DevPaul] Add gitignore : cache file

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -33,55 +33,51 @@ bool is_valid_user_buffer(void *buffer, unsigned size);
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
-void
-syscall_init (void) {
-	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
-			((uint64_t)SEL_KCSEG) << 32);
+void syscall_init (void) {
+	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48 | ((uint64_t)SEL_KCSEG) << 32);
 	write_msr(MSR_LSTAR, (uint64_t) syscall_entry);
 
 	/* The interrupt service rountine should not serve any interrupts
 	 * until the syscall_entry swaps the userland stack to the kernel
 	 * mode stack. Therefore, we masked the FLAG_FL. */
-	write_msr(MSR_SYSCALL_MASK,
-			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+	write_msr(MSR_SYSCALL_MASK, FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
 
 /* The main system call interface */
-void
-syscall_handler (struct intr_frame *f UNUSED) {
+void syscall_handler (struct intr_frame *f UNUSED) {
 	// TODO: Your implementation goes here.
 	switch (f->R.rax) {
-        case SYS_HALT:
-            break;
-        case SYS_EXIT:
+    case SYS_HALT:
+      break;
+    case SYS_EXIT:
 			int status = f->R.rdi;
 			thread_current()->exit_status = status;
 			printf("%s: exit(%d)\n", thread_current()->name, status);
 			thread_exit();
-            break;
-        case SYS_WRITE:  
-            // 이 안에 `write` 시스템 콜의 상세 기능을 구현합니다.
-            // 1. f->R.rdi, f->R.rsi, f->R.rdx에서 인자(fd, buffer, size)를 가져옵니다.
-			int fd = f->R.rdi;
-			void* buffer = f->R.rsi;
-			unsigned size = f->R.rdx;
-			
-            // 2. buffer 포인터가 유효한지 검사합니다.			
-			// 3. fd가 1(콘솔 출력)인지, 일반 파일인지 구분합니다.
-			if (fd == 1) {
-				if (is_valid_user_buffer(buffer, size)) {
-					putbuf(buffer, size);
-					f->R.rax = size;
-				}
-			} else {
-				// 4. 각 상황에 맞게 데이터를 씁니다.
-				// 5. 쓴 바이트 수를 f->R.rax에 저장해서 반환합니다.
-				f->R.rax;
+      break;
+  case SYS_WRITE:  
+    // 이 안에 `write` 시스템 콜의 상세 기능을 구현합니다.
+    // 1. f->R.rdi, f->R.rsi, f->R.rdx에서 인자(fd, buffer, size)를 가져옵니다.
+		int fd = f->R.rdi;
+		void* buffer = f->R.rsi;
+		unsigned size = f->R.rdx;
+		
+    // 2. buffer 포인터가 유효한지 검사합니다.			
+		// 3. fd가 1(콘솔 출력)인지, 일반 파일인지 구분합니다.
+		if (fd == 1) {
+			if (is_valid_user_buffer(buffer, size)) {
+				putbuf(buffer, size);
+				f->R.rax = size;
 			}
-            break;
-        default:
-            break;
-    }
+		} else {
+			// 4. 각 상황에 맞게 데이터를 씁니다.
+			// 5. 쓴 바이트 수를 f->R.rax에 저장해서 반환합니다.
+			f->R.rax;
+		}
+    break;
+  default:
+    break;
+  }
 
 	// thread_exit ();
 }
